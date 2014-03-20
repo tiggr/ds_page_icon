1f7d93ab8d3e9a1b043d36c2edf7acc4cf183c04
Daniel Schöne <daniel@schoene.it>
2014-03-20 18:51:29 +0100

    Adds icon field to TCA / DB

--- ext_tables.php
+++ ext_tables.php
@@ -3,6 +3,33 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'page icon');
+t3lib_div::loadTCA('pages');
 
+$tempColumns = array(
+	'tx_dspageicon_icon' => array(
+		'exclude' => 0,
+		'label'   => 'LLL:EXT:ds_page_icon/Resources/Private/Language/locallang_db.xlf:ds_page_icon.ds_page_icon_icon',
+		'config'  => array(
+			'type'               => 'select',
+			'items'              => Array(
+				Array('', 0),
+			),
+			'fileFolder'         => 'EXT:ds_page_icon/Resources/Public/Icons',
+			'fileFolder_extList' => 'png',
+			'selicon_cols'       => 8,
+			'iconsInOptionTags'  => 1, // doesn't display but...
+			'size'               => 1,
+			'maxitems'           => 1,
+		)
+	)
+);
+
+t3lib_extMgm::addTCAcolumns('pages', $tempColumns, 1);
+t3lib_extMgm::addFieldsToPalette(
+	'pages',
+	'layout',
+	'--linebreak--, tx_dspageicon_icon;;;;1-1-1',
+	'after:backend_layout_next_level');
+
+unset($tempColumns);
 ?>
---
+++ ext_tables.sql
@@ -0,0 +1,6 @@
+#
+# Table structure for table 'pages'
+#
+CREATE TABLE pages (
+	tx_dspageicon_icon tinytext,
+);
